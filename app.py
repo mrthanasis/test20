27dc04958123c51ef954157a39934b8818a589df
Thanasis <mrthanasis@hotmail.com>
2019-10-13 21:31:58 +0300

    New Changes

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-cwd = os.getcwd()
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="My-Project-2020-c7c568ac149d.json"
 import google.auth
@@ -12,7 +11,7 @@ import numpy as np
 def lastdaysresults(x):
     x=int(x)
     query=("""
-    select a.UserType,a.deviceCategory, sum(a.transactions) as Conversions, sum(a.visits) as Impressions,
+    select a.UserType,a.deviceCategory, sum(a.transactions) as Conversions, sum(a.visits) as Sessions,
     sum(a.transactions)/ sum(a.visits) as Convertion_rate , a.lastdays, a.date
     from (select case when visitNumber=1 then 'New' else 'Return' end as UserType, device.deviceCategory, totals.transactions, totals.visits,
     DENSE_RANK() OVER(ORDER BY CAST(date AS INT64) DESC) AS lastdays, date
@@ -26,15 +25,15 @@ def lastdaysresults(x):
     location="US")
     df=query_job.result().to_dataframe()
     
-    lastxdays=df.groupby(['lastdays','date']).sum().eval("Conversion_rate=Conversions/Impressions")\
+    lastxdays=df.groupby(['lastdays','date']).sum().eval("Conversion_rate=Conversions/Sessions")\
     .query('lastdays<={}'.format(x)).reset_index().set_index('date')[['Conversion_rate']]
     
     
     periods=df.assign(Period=np.where(df['lastdays']<=x,'ThisPeriod','PastPeriod')).groupby('Period').sum()\
-    .eval("Conversion_rate=Conversions/Impressions")[['Conversion_rate']]
+    .eval("Conversion_rate=Conversions/Sessions")[['Conversion_rate']]
     
     breakdown=df.assign(Period=np.where(df['lastdays']<=x,'ThisPeriod','PastPeriod')).groupby(['Period','UserType','deviceCategory'])\
-    .sum().eval("Conversion_rate=Conversions/Impressions")[['Conversion_rate']].reset_index()
+    .sum().eval("Conversion_rate=Conversions/Sessions")[['Conversion_rate']].reset_index()
     
     
     return(lastxdays.to_dict(),periods.to_dict(),breakdown.to_dict())
